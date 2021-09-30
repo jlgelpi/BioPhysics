eaa4a6b777ca3238240172b50153dd753e1efe31
Josep Ll. Gelpi <gelpi@ub.edu>
2021-09-30 18:41:10 +0200

    Fixed shebang line

--- Examples/ex_distances_2res.py
+++ Examples/ex_distances_2res.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3
+#!/usr/bin/env python
 #
 """ Simple program to print distances between atoms """
 
--- basic_setup.py
+++ basic_setup.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3
+#!/usr/bin/env python
 
 """
     Initial setup a structure for Energy evaluation
